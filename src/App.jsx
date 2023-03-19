031f1b4d96187e3f6e7ea4abe74a320b4c53cd6c
ogzCode <oguzguc44@gmail.com>
2023-03-19 12:09:36 +0300

    Added main styles and edited main component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import Header from './components/Header/Header';
+function App() {
+  return (
+    <div className='min-w-screen min-h-screen'>
+      <Header/>
+    </div>
+  )
+}
+
+export default App
