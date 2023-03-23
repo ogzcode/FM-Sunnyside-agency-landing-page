462cd5fc13631e781387d72fba38ac5726c3b82e
ogzCode <oguzguc44@gmail.com>
2023-03-23 11:29:57 +0300

    Main component and style added to main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+
 function App() {
   return (
     <div className='min-w-screen min-h-screen'>
-      <Header/>
+      <Header />
+      <Main />
     </div>
   )
 }
