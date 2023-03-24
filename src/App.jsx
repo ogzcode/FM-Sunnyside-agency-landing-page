eaab32951740b728d75f2e15f2ed49ae52952432
ogzCode <oguzguc44@gmail.com>
2023-03-24 12:51:00 +0300

    Testimonial component added to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
+import TestimonialSection from './components/Testimonial/TestimonialSection';
 
 function App() {
   return (
     <div className='min-w-screen min-h-screen'>
       <Header />
       <Main />
+      <TestimonialSection />
     </div>
   )
 }
