91090b4fc64eb70d59ce29087ba98cc96869e7a1
ogzCode <oguzguc44@gmail.com>
2023-03-28 11:30:34 +0300

    Mobile menu imported

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import bgHeader from "../../images/desktop/image-header.jpg";
 import arrowDown from "../../images/icon-arrow-down.svg";
-import NavDesktop from "./NavDesktop";
+import Nav from "./Nav";
 
 function Header(){
   return (
     <header className="h-[100vh] bg-center bg-cover" style={{backgroundImage: `url(${bgHeader})`}}>
-      <NavDesktop/>
+      <Nav/>
       <div className='flex flex-col items-center mt-6'>
         <h1 className='text-white text-6xl mb-20 font-fraunces text-center'>We are creatives</h1>
         <img src={arrowDown} className="w-[32px]"/>
